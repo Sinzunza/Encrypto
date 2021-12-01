851a0e754a71d821666043d849da2c4a5fe33147
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-12-01 08:39:40 -0800

    Update mainwindow.h

--- headers/mainwindow.h
+++ headers/mainwindow.h
@@ -10,8 +10,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
+
     Q_OBJECT
 
 public:
