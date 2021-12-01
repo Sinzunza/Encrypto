065fa3bba4705ad1ec3bc9cd14e613ff302616b7
Sergio Inzunza <56137371+Sinzunza@users.noreply.github.com>
2021-12-01 08:35:41 -0800

    Update mathfncts.cpp

--- sources/mathfncts.cpp
+++ sources/mathfncts.cpp
@@ -3,34 +3,33 @@
 
 using namespace std;
 
-mathFncts::mathFncts()
-{
+mathFncts::mathFncts() {
     //ctor
 }
 
-unsigned long long int mathFncts::gcd (unsigned long long int a, unsigned long long int b){
-    if(a == b){
+unsigned long long int mathFncts::gcd(unsigned long long int a, unsigned long long int b) {
+    if (a == b) {
         return a;
     }
-    if(a < b){
+    else if (a < b) {
         swap(a,b);
     }
     return gcd(a-b,b);
 }
 
 //simplifies modding numbers to the power of several dozen digits if needed
-int mathFncts::modBigNumber(unsigned long long int number, unsigned long long int power, unsigned long long int modder){
+int mathFncts::modBigNumber(unsigned long long int number, unsigned long long int power, unsigned long long int modder) {
     vector<unsigned long long int> leftOvers;
-    while(power > 1){
-        if(power % 2 == 1){
+    while (power > 1) {
+        if (power % 2 == 1) {
             leftOvers.push_back(number);
         }
         number *= number;
         number %= modder;
         power /= 2;
     }
-    if(leftOvers.size() > 0){
-        for(unsigned long long int i = 0; i < leftOvers.size(); i++){
+    if (leftOvers.size() > 0) {
+        for (unsigned long long int i = 0; i < leftOvers.size(); i++) {
             number *= leftOvers.at(i);
             number %= modder;
         }
